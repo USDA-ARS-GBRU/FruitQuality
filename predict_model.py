fb5fb95a7320c6aadb85ad8e6949f5b2b60da21b
Mitanshu Reshamwala <42906426+MitanshuShaBa@users.noreply.github.com>
2023-06-21 07:50:56 -0400

    add predicting script using wandb model

--- predict_model.py
+++ predict_model.py
@@ -26,8 +26,14 @@ def image_to_same_shape(image, height, width):
                          channels), color, dtype=np.uint8)
 
     # compute center offset
-    x_center = (new_image_width - old_image_width) // 2
-    y_center = (new_image_height - old_image_height) // 2
+    x_center = abs(new_image_width - old_image_width) // 2
+    y_center = abs(new_image_height - old_image_height) // 2
+
+    # Return cropped image if orginal image was larger
+    if old_image_height > new_image_height and old_image_width > new_image_width:
+        result = image[old_image_height//2-height//2: old_image_height//2 +
+                       height//2, old_image_width//2-width//2: old_image_width//2+width//2]
+        return result
 
     # copy img image into center of result image
     result[y_center:y_center+old_image_height,
@@ -37,18 +43,6 @@ def image_to_same_shape(image, height, width):
 
 
 class Dataset:
-    """Dataset. Read images, apply augmentation and preprocessing transformations.
-
-    Args:
-        images_dir (str): path to images folder
-        masks_dir (str): path to segmentation masks folder
-        class_values (list): values of classes to extract from segmentation mask
-        augmentation (albumentations.Compose): data transfromation pipeline 
-            (e.g. flip, scale, etc.)
-        preprocessing (albumentations.Compose): data preprocessing 
-            (e.g. noralization, shape manipulation, etc.)
-
-    """
 
     CLASSES = ['unlabelled', 'seed', 'pulp', 'albedo', 'flavedo']
 
@@ -85,7 +79,7 @@ class Dataset:
         image = image_to_same_shape(image, 1024, 1024)
         mask = image_to_same_shape(mask, 1024, 1024)
 
-        # extract certain classes from mask (e.g. cars)
+        # extract certain classes from mask
         masks = [(mask == v*(255//4)) for v in self.class_values]
         mask = np.stack(masks, axis=-1).astype('float')
 
@@ -110,69 +104,12 @@ class Dataset:
         return len(self.ids)
 
 
-class Dataloder(keras.utils.Sequence):
-    """Load data from dataset and form batches
-
-    Args:
-        dataset: instance of Dataset class for image loading and preprocessing.
-        batch_size: Integet number of images in batch.
-        shuffle: Boolean, if `True` shuffle image indexes each epoch.
-    """
-
-    def __init__(self, dataset, batch_size=1, shuffle=False):
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.indexes = np.arange(len(dataset))
-
-        self.on_epoch_end()
-
-    def __getitem__(self, i):
-
-        # collect batch data
-        start = i * self.batch_size
-        stop = (i + 1) * self.batch_size
-        data = []
-        for j in range(start, stop):
-            data.append(self.dataset[j])
-
-        # transpose list of lists
-        batch = [np.stack(samples, axis=0) for samples in zip(*data)]
-
-        return batch
-
-    def __len__(self):
-        """Denotes the number of batches per epoch"""
-        return len(self.indexes) // self.batch_size
-
-    def on_epoch_end(self):
-        """Callback function to shuffle indexes each epoch"""
-        if self.shuffle:
-            self.indexes = np.random.permutation(self.indexes)
-
-
 api = wandb.Api()
 artifact_path = 'mresham/fruit-quality/likely-sweep-41_model:v0'
 artifact = api.artifact(artifact_path)
 artifact.download()
 path = "artifacts/" + artifact_path.split("/")[-1]
 model = tf.keras.saving.load_model(path, compile=False)
-
-DATA_DIR = './Images/'
-MASK_DIR = './Masks/'
-CLASSES = ['seed', 'pulp', 'albedo', 'flavedo']
-
-data_ids = [image_id.replace(".png", "") for image_id in os.listdir(MASK_DIR)]
-SIZE = len(data_ids)
-TRAIN_SIZE = int(0.6 * SIZE)
-VAL_SIZE = int(0.2 * SIZE)
-test_dataset = Dataset(
-    data_ids[TRAIN_SIZE+VAL_SIZE:],
-    DATA_DIR,
-    MASK_DIR,
-    classes=CLASSES,
-)
-test_dataloader = Dataloder(test_dataset, batch_size=1, shuffle=False)
 
 optim = keras.optimizers.Adam(0.0001)
 dice_loss = sm.losses.DiceLoss()
@@ -183,5 +120,20 @@ metrics = [sm.metrics.IOUScore(threshold=0.5),
            sm.metrics.FScore(threshold=0.5)]
 
 model.compile(optim, total_loss, metrics)
-scores = model.evaluate(test_dataloader)
-print(scores)
+
+image_path = "tmp/inputs/IMG_1287_top.jpeg"
+image = cv2.imread(image_path)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image = image_to_same_shape(image, 1024, 1024)
+
+preds = model.predict(image[tf.newaxis, ...])
+
+
+def create_mask(pred_mask):
+  pred_mask = tf.math.argmax(pred_mask, axis=-1)
+  pred_mask = pred_mask[..., tf.newaxis]
+  pred_mask *= 255//5
+  return pred_mask[0]
+
+cv2.imwrite("tmp/outputs/IMG_1287_top_Mask.png", create_mask(preds).numpy())
+
